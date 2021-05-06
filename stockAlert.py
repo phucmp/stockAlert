42229cb3a32de17c74ff98f510672363f2fdf3d2
phucmp <phucmp@uci.edu>
2021-05-05 23:26:09 -0700

    update stock alert with finnhub api calls

--- stockAlert.py
+++ stockAlert.py
@@ -1,7 +1,38 @@
-import time 
+import os
+import websocket
+import requests
+from dotenv import load_dotenv
 from datetime import datetime
 
+class Trades:
+    def __init__(self, ticker, token):
+        self.url = "wss://ws.finnhub.io?token=" + token
+        self.ticker = ticker
+
+    def _on_message(self, ws, message):
+        print(message)
+
+    def _on_error(self, ws, error):
+        print(error)
+
+    def _on_close(self, ws):
+        print("### Closed ###")
+
+    def _on_open(self, ws):
+        ws.send('{"type":"subscribe","symbol":"' + self.ticker + '"}')
+
+    def start(self):
+        websocket.enableTrace(True)
+        ws = websocket.WebSocketApp(self.url, on_message=self._on_message, on_error=self._on_error, on_close=self._on_close)
+        ws.on_open = self._on_open
+        ws.run_forever()
+
 class Program: 
+    def __init__(self):
+        load_dotenv()
+        self.api_token = os.environ.get("FINNHUB_API_KEY")
+        self.api_uri = 'https://finnhub.io/api/v1/quote?symbol='
+
     def get_local_time(self):
         return datetime.now()
 
@@ -14,12 +45,18 @@ class Program:
     def is_market_open(self):
         return self.get_local_time() >= self.get_market_open_time() and self.get_local_time() < self.get_market_close_time()
 
+    def get_quote(self):
+        url = self.api_uri + self.ticker + '&token=' + self.api_token
+        r = requests.get(url)
+        print(r.json())
+
     def start(self):
-        self.ticker = raw_input("Enter Your Stock Ticker: ")
-        while(self.is_market_open()):
-            print("Market is open")
-            print("Tracking: $" + self.ticker)
-            time.sleep(20)
+        self.ticker = input("Enter Your Stock Ticker: ").upper()
+        if (self.is_market_open()):
+            trades = Trades(self.ticker, self.api_token)
+            trades.start()
+        else:
+            self.get_quote()
 
 if __name__ == "__main__":
     program = Program()
